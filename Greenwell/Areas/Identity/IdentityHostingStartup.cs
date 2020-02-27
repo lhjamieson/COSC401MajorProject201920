71fe49fa1e28024f9bf274231367c67090a0d8ef
Thomas Walker <thwalker@smcm.edu>
2020-02-27 12:32:39 -0500

    Added Scaffolded User Auth/Files

---
+++ Greenwell/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Greenwell.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Greenwell.Areas.Identity.IdentityHostingStartup))]
+namespace Greenwell.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<GreenwellUserContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("GreenwellUserContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<GreenwellUserContext>();
+            });
+        }
+    }
+}
