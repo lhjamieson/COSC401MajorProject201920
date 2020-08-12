6f3e4ed7b51ac0bb2fb13905288bbd724a6e65fb
Ahkhan81 <43010537+Ahkhan81@users.noreply.github.com>
2020-08-12 09:18:13 -0400

    Updated for Hostgator

---
+++ Greenwell/ClientApp/build/precache-manifest.025579d46779b274ea1c85efab9d6758.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6944701f22bc2a13c390e0769b4b9f07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "316a2e53cd58a4d23462",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "9c328573554076bebf7f",
+    "url": "/static/css/main.6c298894.chunk.css"
+  },
+  {
+    "revision": "316a2e53cd58a4d23462",
+    "url": "/static/js/2.60b04534.chunk.js"
+  },
+  {
+    "revision": "1b1e40b5beae77731aca447ee539ba2f",
+    "url": "/static/js/2.60b04534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c328573554076bebf7f",
+    "url": "/static/js/main.e4f13e7a.chunk.js"
+  },
+  {
+    "revision": "e535b9ac2fe35e57e820",
+    "url": "/static/js/runtime-main.f94fca06.js"
+  },
+  {
+    "revision": "52fdeaadad06db20cb18d924b1d0e851",
+    "url": "/static/media/navbar.52fdeaad.png"
+  }
+]);
